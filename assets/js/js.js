c2bafe40919f375fbb152faec44d80d17ea0fbee
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-07 12:02:56 -0500

    adding member to bottom form with local storage

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,11 +1,28 @@
 // alert("hi");
 
-var name = "";
-var phoneNumber = "";
-var email = "";
-
 $(document).ready(function(){
 	$("#submitButton").on("click", function(){
-		name =  $("#nameInput").append("#nameOutput");
+		// Storing user input into variables
+		name = $("#nameInput").val();
+		phoneNumber = $("#phoneNumberInput").val();
+		email = $("#emailInput").val(); 
+		
+		console.log(name, phoneNumber, email);
+		
+			$("#nameOutput").html(name);
+			$("#phoneNumberOutput").html(phoneNumber);
+			$("#emailOutput").html(email);
+
+					// Saving ... to local storage
+					localStorage.setItem('name', name);
+					localStorage.setItem('phoneNumber', phoneNumber);
+					localStorage.setItem('email', email);
+
+					return false;
 	});
 });
+
+//Shows whats saved to local storage
+$("#nameInput").html(localStorage.getItem('name'));
+$("#phoneNumberInput").html(localStorage.getItem("phoneNumber"));
+$("#emailInput").html(localStorage.getItem('email'));

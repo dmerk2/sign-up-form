42764b98527a346fe8d447a4c172ca2828849402
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-07 13:25:14 -0500

    changed from localstorage to .append userInput

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,27 +2,39 @@
 
 $(document).ready(function(){
 	$("#submitButton").on("click", function(){
+
 		// Storing user input into variables
 		name = $("#nameInput").val();
 		phoneNumber = $("#phoneNumberInput").val();
 		email = $("#emailInput").val(); 
 		
 		console.log(name, phoneNumber, email);
-		
+			
+			// Placing variables(user inputs) into html
 			$("#nameOutput").html(name);
 			$("#phoneNumberOutput").html(phoneNumber);
 			$("#emailOutput").html(email);
 
-					// Saving ... to local storage
-					localStorage.setItem('name', name);
-					localStorage.setItem('phoneNumber', phoneNumber);
-					localStorage.setItem('email', email);
+				// Puts variable name into nameOutput
+				// $("#nameOutput").append(name);
+				// $("#phoneNumberOutput").append(phoneNumber);
+				// $("#emailOutput").append(email);
+				
+				// Clears inputs
+				$("#nameInput").val("");
+				$("#phoneNumberInput").val("");
+				$("#emailInput").val("");
 
-					return false;
+					// Saving to local storage
+					// localStorage.setItem('name', name);
+					// localStorage.setItem('phoneNumber', phoneNumber);
+					// localStorage.setItem('email', email);
+
+					 return false;
 	});
 });
 
-//Shows whats saved to local storage
-$("#nameInput").html(localStorage.getItem('name'));
-$("#phoneNumberInput").html(localStorage.getItem("phoneNumber"));
-$("#emailInput").html(localStorage.getItem('email'));
+// Shows whats saved to local storage 
+// $("#nameInput").html(localStorage.getItem('name'));
+// $("#phoneNumberInput").html(localStorage.getItem("phoneNumber"));
+// $("#emailInput").html(localStorage.getItem('email'));
